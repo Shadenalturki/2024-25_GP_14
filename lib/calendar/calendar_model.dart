f2146dbf12b478b012ad9bbdf2f735d7ce42f9e6
Shadenalturki <shadenalturki2003@gmail.com>
2024-11-11 02:47:46 +0300

    last update

--- lib/calendar/calendar_model.dart
+++ lib/calendar/calendar_model.dart
@@ -1,13 +1,40 @@
+import '/flutter_flow/flutter_flow_button_tabbar.dart';
+import '/flutter_flow/flutter_flow_calendar.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
 import 'calendar_widget.dart' show CalendarWidget;
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 class CalendarModel extends FlutterFlowModel<CalendarWidget> {
-  @override
-  void initState(BuildContext context) {}
+  ///  State fields for stateful widgets in this page.
+
+  // State field(s) for TabBar widget.
+  TabController? tabBarController;
+  int get tabBarCurrentIndex =>
+      tabBarController != null ? tabBarController!.index : 0;
+
+  // State field(s) for Calendar widget.
+  DateTimeRange? calendarSelectedDay1;
+  // State field(s) for Calendar widget.
+  DateTimeRange? calendarSelectedDay2;
 
   @override
-  void dispose() {}
+  void initState(BuildContext context) {
+    calendarSelectedDay1 = DateTimeRange(
+      start: DateTime.now().startOfDay,
+      end: DateTime.now().endOfDay,
+    );
+    calendarSelectedDay2 = DateTimeRange(
+      start: DateTime.now().startOfDay,
+      end: DateTime.now().endOfDay,
+    );
+  }
+
+  @override
+  void dispose() {
+    tabBarController?.dispose();
+  }
 }
-
-// shaden editted this
